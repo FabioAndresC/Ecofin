cce4309cdb663cc0101e9304c7cc099a5b158bfc
Fabio-Crespo <a2020115158@estudiantes.upsa.edu.bo>
2024-06-24 23:58:24 -0400

    delete api folder

--- api/utils/error.ts
+++
@@ -1,6 +0,0 @@
-export const CreateError = (status: number, message: string) => {
-    const error: any = new Error();
-    error.status = status;
-    error.message = message;
-    return error;
-};
--- api/utils/success.ts
+++
@@ -1,8 +0,0 @@
-export const CreateSuccess = (status: number, message: string, data?: any) => {
-    const successObj = {
-        status: status,
-        message: message,
-        data: data,
-    };
-    return successObj;
-};
