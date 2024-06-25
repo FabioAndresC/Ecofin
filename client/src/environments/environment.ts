ba5bbb73655aacb0ff2a9bfaf83d5604070963b8
Fabio-Crespo <a2020115158@estudiantes.upsa.edu.bo>
2024-06-25 01:15:52 -0400

    fix: some fixex

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,7 +1,6 @@
-
-  export const environment = {
-    supabaseUrl: 'https://fhversoornsguqvselbp.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZodmVyc29vcm5zZ3VxdnNlbGJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ4NTc1NzEsImV4cCI6MjAzMDQzMzU3MX0.EB9QONpfR8CUByCA71V_N0ovaywg-w0Yfc4AG-FXMJM',
-    geminiApiKey: 'undefined',
-    apifyApiKey: 'undefined'
-  };
+export const environment = {
+    supabaseUrl: process.env["SUPABASE_URL"],
+    supabaseKey: process.env["SUPABASE_KEY"],
+    geminiApiKey: process.env["GEMINI_API_KEY"],
+    apifyApiKey: process.env["APIFY_API_KEY"]
+};
